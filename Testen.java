ba13e5ff0c975adedc8afbd4c404349094ff184e
Martin Weber <mweber02@student.tgm.ac.at>
2014-12-05 11:58:54 +0100

    Methoden in Testen hinzugefuegt

--- Testen.java
+++ Testen.java
@@ -13,8 +13,12 @@ public class Testen {
 	 * @param args
 	 */
 	public static void main(String[]args){
+		/*
+		 * Test von Zahlen
+		 */
 		Zahlen test2=new Zahlen(2.0);
 		test2.add(1.0);
+		System.out.println("Testen der Zahlen Klasse: ");
 		System.out.println("Das Maximum der Zahlen: "+test2.max());
 		System.out.println("Das Mimimum der Zahlen: "+test2.min());
 		System.out.println("Die Summe der Zahlen: "+test2.summe());
@@ -22,6 +26,7 @@ public class Testen {
 		/*
 		 * Test von Rauminhalt
 		 */
+		System.out.println("Testem der Rauminhalt Klasse: ");
 		System.out.println("Volumen eines Kegels mit Grundflaeche und Hoehe Gegeben: "+Rauminhalt.coneGH(20, 5));
 		System.out.println("Volumen eines Kegels mit Radius und hoehe gegeben "+Rauminhalt.conePIrh(10, 5));
 		System.out.println("Volumen eines Quaders mit a b und h gegeben "+Rauminhalt.cuboid(2,2,4));
@@ -30,6 +35,7 @@ public class Testen {
 		/*
 		 * testung der klasse flaechen
 		 */
+		System.out.println("Testen der Flaechen Klasse: ");
 		Flaechen f1 = new Flaechen();
 		System.out.println("Die Flaeche eines Rechtecks: "+f1.flaecheR(5, 9));
 		System.out.println("Die Flache eines Kreises: "+f1.flaecheK(4));
