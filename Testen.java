9a6487f188f73ebfcbadaf9acd7a0d515f2255c7
Martin Weber <mweber02@student.tgm.ac.at>
2014-12-05 11:18:35 +0100

    Steinwenders Klasse und Zahlen zum testen hinzugefuegt

--- Testen.java
+++ Testen.java
@@ -20,16 +20,13 @@ public class Testen {
 		System.out.println("Die Summe der Zahlen: "+test2.summe());
 		
 		Rauminhalt test1= new Rauminhalt();
-		
-<<<<<<< HEAD
-=======
+
 		/*
 		 * testung der klasse flaechen
 		 */
 		Flaechen f1 = new Flaechen();
-		System.out.println(f1.flaecheR(5, 9));
-		System.out.println(f1.flaecheK(4));
-		System.out.println(f1.flaecheD(2, 5, 4));
->>>>>>> dbb03c9f9278a1b2fded8c9222412929eccb2f15
+		System.out.println("Die Flaeche eines Rechtecks: "+f1.flaecheR(5, 9));
+		System.out.println("Die Flache eines Kreises: "+f1.flaecheK(4));
+		System.out.println("Die Flaeche eines Dreiecks: "+f1.flaecheD(2, 5, 4));
 	}
 }
