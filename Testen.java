dbb03c9f9278a1b2fded8c9222412929eccb2f15
Jan-Philipp Steinwender <steinwenderjan@hotmail.com>
2014-12-05 11:10:56 +0100

    zu testeten methoden von flaeche hinzugefügt

--- Testen.java
+++ Testen.java
@@ -15,5 +15,13 @@ public class Testen {
 	public static void main(String[]args){
 		
 		Rauminhalt test1= new Rauminhalt();
+		
+		/*
+		 * testung der klasse flaechen
+		 */
+		Flaechen f1 = new Flaechen();
+		System.out.println(f1.flaecheR(5, 9));
+		System.out.println(f1.flaecheK(4));
+		System.out.println(f1.flaecheD(2, 5, 4));
 	}
 }
