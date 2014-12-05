90c08daf832a1e0156d32a274ca85e8ab6f758bb
Martin Weber <mweber02@student.tgm.ac.at>
2014-12-05 12:22:54 +0100

    Ausgabe verschoenert

--- Testen.java
+++ Testen.java
@@ -12,33 +12,43 @@ public class Testen {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[]args){
+	public static void main(String[] args) {
 		/*
 		 * Test von Zahlen
 		 */
-		Zahlen test2=new Zahlen(2.0);
+		Zahlen test2 = new Zahlen(2.0);
 		test2.add(1.0);
 		System.out.println("Testen der Zahlen Klasse: ");
-		System.out.println("Das Maximum der Zahlen: "+test2.max());
-		System.out.println("Das Mimimum der Zahlen: "+test2.min());
-		System.out.println("Die Summe der Zahlen: "+test2.summe());
-		
+		System.out.println("Das Maximum der Zahlen: " + test2.max());
+		System.out.println("Das Mimimum der Zahlen: " + test2.min());
+		System.out.println("Die Summe der Zahlen: " + test2.summe());
+
 		/*
 		 * Test von Rauminhalt
 		 */
 		System.out.println("Testem der Rauminhalt Klasse: ");
-		System.out.println("Volumen eines Kegels mit Grundflaeche und Hoehe Gegeben: "+Rauminhalt.coneGH(20, 5));
-		System.out.println("Volumen eines Kegels mit Radius und hoehe gegeben "+Rauminhalt.conePIrh(10, 5));
-		System.out.println("Volumen eines Quaders mit a b und h gegeben "+Rauminhalt.cuboid(2,2,4));
-		System.out.println("Volumen einer Pyramide: "+Rauminhalt.pyramid(10, 5));
+		System.out
+				.println("Volumen eines Kegels mit Grundflaeche=20 und Hoehe=5 Gegeben: "
+						+ Rauminhalt.coneGH(20, 5));
+		System.out
+				.println("Volumen eines Kegels mit Radius=10 und Hoehe=5 gegeben "
+						+ Rauminhalt.conePIrh(10, 5));
+		System.out.println("Volumen eines Quaders mit a=2 b=2 und h=4 gegeben "
+				+ Rauminhalt.cuboid(2, 2, 4));
+		System.out
+				.println("Volumen einer Pyramide mit der Grundflaeche=10 und Hoehe=5: "
+						+ Rauminhalt.pyramid(10, 5));
 
 		/*
 		 * testung der klasse flaechen
 		 */
 		System.out.println("Testen der Flaechen Klasse: ");
 		Flaechen f1 = new Flaechen();
-		System.out.println("Die Flaeche eines Rechtecks: "+f1.flaecheR(5, 9));
-		System.out.println("Die Flache eines Kreises: "+f1.flaecheK(4));
-		System.out.println("Die Flaeche eines Dreiecks: "+f1.flaecheD(2, 5, 4));
+		System.out.println("Die Flaeche eines Rechtecks mit a=5 und b=9: "
+				+ f1.flaecheR(5, 9));
+		System.out.println("Die Flache eines Kreises mit r=4: "
+				+ f1.flaecheK(4));
+		System.out.println("Die Flaeche eines Dreiecks mit a=2 b=5 und c=4: "
+				+ f1.flaecheD(2, 5, 4));
 	}
 }
