b984627d59dafbd7fa523e94f97b4fa2e290f0fb
Martin Weber <mweber02@student.tgm.ac.at>
2014-12-05 09:53:59 +0100

    Dateien aktualisiert

--- Testen.java
+++ Testen.java
@@ -1,5 +1,14 @@
 package weber_straeszler_steinwender;
 
+/**
+ * Klasse in der die Funktionen der anderen Klassen getestet werden
+ * 
+ * @author Martin
+ * @version 1.0
+ */
 public class Testen {
 
+	public static void main(String[]args){
+		
+	}
 }
